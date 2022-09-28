90ddfad724b066e8d15003587bc119203b8f636d
Alexjrfy <alexjr_mendess@hotmail.com>
2022-09-28 00:17:21 -0300

    Aplicação + Crud Funcional

---
+++ src/DevIO.API/Configuration/DepedencyInjectionConfig.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.API.Configuration
+{
+    public static class DepedencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDepencencies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            
+            services.AddScoped<INotificador, Notificador>();
+
+            return services;
+        }
+    }
+}
---
+++ src/DevIO.API/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.API.Controllers
+{
+    public abstract class MainController : ControllerBase
+    {
+        //validações de notificações e erros
+
+        //validação de modelstate
+
+        //validação de operação
+    }
+}
